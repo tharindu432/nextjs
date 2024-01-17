2ac0822a1d8d03481696985942cbf3dd5843ada5
tharindu432 <tharinduchaturanga432@gmail.com>
2024-01-17 12:29:51 +0530

    navigate through Link & module.css files created

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import {Routes, Route } from 'react-router-dom';
 import AllMeetups from './pages/AllMeetups';
 import Favorites from './pages/Favorites';
 import NewMeetups from './pages/NewMeetup';
+import MainNavigation from './components/layout/MainNavigation'
 function App() {
   //localhost:3000
   //after deployment -->my-page.com
  
   return (
+
+    
     
     <div>
+      <MainNavigation/>
     <Routes>
       <Route path="/" element={<AllMeetups />} />
       <Route path="/new-meetup" element={<NewMeetups />} />
